8dafcc024a7bd1b78ad6f1dfbea97ad38f918eb8
EmmanuelSteven <chizzyjiver@gmail.com>
2024-02-21 07:29:31 +0100

    fixed errors

--- main.js
+++ main.js
@@ -73,21 +73,31 @@ if (search === undefined){
     search.item += 1;
 }
 
-localStorage.setItem("data", JSON.stringify(basket))
 //  console.log (basket)
  update(selectedItem.id);
+
+localStorage.setItem("data", JSON.stringify(basket))
+
 };
+
  let decrement = (id) => {
     let selectedItem = id;
 let search = basket.find((x) => x.id === selectedItem.id);
-if (search.item === 0) return;
+if (search === undefined) return;
+
+else if (search.item === 0) return;
+
  else {
     search.item -= 1;
 }
 
-localStorage.setItem("data", JSON.stringify(basket))
+update(selectedItem.id);
+
+basket = basket.filter((x) => x.item !==0);
 //  console.log (basket)
- update(selectedItem.id);
+
+
+ localStorage.setItem("data", JSON.stringify(basket))
 
  };
 
